4cad98f4f7926697257d3b8cf2298107d10fee35
artem88 <artemik2010@gmail.com>
2018-04-14 09:11:00 +0300

    Created Scenes with 360 Skyboxes and Sings
    Added script and Event Trigger (Pointer Click) for transitions between Scenes
    
    Target Platform: Android
    
    XR Settings: Cardboard SDK

---
+++ Assets/Scripts/LoadAnotherScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAnotherScene : MonoBehaviour {
+
+	// Load Scene
+    public string sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
